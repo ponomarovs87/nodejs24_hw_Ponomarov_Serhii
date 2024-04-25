bd92cc71776b940df87bd74da8a12e237d927cc2
ponomarovs87 <ponomarovs87@gmail.com>
2024-04-25 20:51:00 +0200

    add difficult colors && difficult logs && del npm Colors

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const logger = require('./utils/logger')('main');
+const logger = require("./utils/logger")("main");
 
-logger.info('the script is running!');
-logger.warn('the script is running!');
-logger.error('the script is running!');
-
+logger.info("the script is running!");
+logger.warn("the script is running!");
+logger.error("the script is running!");
+logger.info("the script is running!", 2, 345, 2);
+logger.error("the script is running!", 2, 345, 2, {}, [1, 2], {
+  name: "Serhii",
+  innerOBJ: {
+    name: "vasya",
+    innerobj: { name: "vasya2", innerarray: [1, "82", { name: "123" }] },
+  },
+});
--- utils/logger.js
+++ utils/logger.js
@@ -1,34 +1,31 @@
-const colors = {
-  reset: "\x1b[0m",
-  red: "\x1b[31m",
-  yellow: "\x1b[33m",
-  blue: "\x1b[34m",
-  magenta: "\x1b[35m",
-};
+const { useTheme, styles } = require("./colors/node-colors");
 
 function info(titleText, msg) {
-  console.log(colors.yellow, `${titleText}:`, colors.blue, msg, colors.reset);
+  console.log(
+    useTheme(`${titleText} :`, styles.backgroundColors.bgYellow),
+    useTheme(msg, styles.backgroundColors.bgBlue)
+  );
 }
 
 function warn(titleText, msg) {
   console.warn(
-    colors.yellow,
-    `${titleText}:`,
-    colors.magenta,
-    msg,
-    colors.reset
+    useTheme(`${titleText} :`, styles.backgroundColors.bgYellow),
+    useTheme(msg, styles.backgroundColors.bgMagenta)
   );
 }
 
 function error(titleText, msg) {
-  console.error(colors.yellow, `${titleText}:`, colors.red, msg, colors.reset);
+  console.error(
+    useTheme(`${titleText} :`, styles.backgroundColors.bgYellow),
+    useTheme(msg, styles.backgroundColors.bgRed)
+  );
 }
 
 function getLogger(titleText) {
   return {
-    info: (msg) => info(titleText, msg),
-    warn: (msg) => warn(titleText, msg),
-    error: (msg) => error(titleText, msg),
+    info: (...args) => info(titleText, args),
+    warn: (...args) => warn(titleText, args),
+    error: (...args) => error(titleText, args),
   };
 }
 
