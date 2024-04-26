536dda756e9f7522fb84e70608a332e9576f58e9
ponomarovs87 <ponomarovs87@gmail.com>
2024-04-27 01:36:11 +0200

    first step

---
+++ docs/notes/hw3_notes.md
@@ -0,0 +1,14 @@
+# Дополнительно:
+
+[Відеозапис заняття](https://youtu.be/7enIHwm79nE?t=723)
+
+---
+
+## Ход выполнения:
+
+1. выполнение [#1](../tasks/hw3_todo.md)
+2. 
+
+## Материалы:
+
+`Node fs Documentation` [ссылка](https://nodejs.org/api/fs.html)
--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const logger = require("./utils/logger")("main");
+const fileSync = require("./file_sync");
+fileSync.start();
 
 logger.info("the script is running!");
 logger.warn("the script is running!");
 logger.error("the script is running!");
 logger.info("the script is running!", 2, 345, 2);
-logger.error("the script is running!", 2, 345, 2, {}, [1, 2], {
-  name: "Serhii",
-  innerOBJ: {
-    name: "vasya",
-    innerobj: { name: "vasya2", innerarray: [1, "82", { name: "123" }] },
-  },
-});
+logger.error(
+  "the script is running!",
+  2,
+  345,
+  2,
+  {},
+  [1, 2],
+  {
+    name: "Serhii",
+    innerObj: {
+      name: "Vasya",
+      innerObj: {
+        name: "Vasya2",
+        innerArray: [1, "82", { name: "123" }],
+      },
+    },
+  }
+);
