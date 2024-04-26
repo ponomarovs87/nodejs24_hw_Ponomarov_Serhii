1593ede78a63cf5a7fd72efc765a2c12e551c99a
ponomarovs87 <ponomarovs87@gmail.com>
2024-04-26 23:25:54 +0200

    bugfix quotes && default name

--- config/default.js
+++ config/default.js
@@ -2,5 +2,5 @@ const useConsoleColors = process.env.COLORS_ENABLED === "1";
 
 module.exports = {
   useConsoleColors,
-  logLevel: process.env.LOG_LEVEL || info,
+  logLevel: process.env.LOG_LEVEL || "warn",
 };
