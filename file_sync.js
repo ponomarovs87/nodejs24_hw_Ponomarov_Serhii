1305a82b1ebc51dd425052f241dfe113d4a608e6
ponomarovs87 <ponomarovs87@gmail.com>
2024-04-27 17:01:27 +0200

    start on h3_№2 task

--- file_sync.js
+++ file_sync.js
@@ -2,8 +2,39 @@ const fsAsync = require("fs/promises");
 const path = require("path");
 
 const fileSync = {
-  start() {
-    console.log(path);
+  async start() {
+    // пути
+    const target = path.join(__dirname, "target");
+    const source = path.join(__dirname, "source");
+    const targetItems = await fsAsync.readdir(target);
+    const sourceItems = await fsAsync.readdir(source);
+    //сравнение массивов по елементам и получем новый массив с елементами которых нет
+    let newArray = [];
+    for (let i = 0; i < targetItems.length; i++) {
+      let found = false;
+      for (let x = 0; x < sourceItems.length; x++) {
+        if (targetItems[i] === sourceItems[x]) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        newArray.push(targetItems[i]);
+      }
+    }
+    console.log(newArray);
+    // выясняем фаил ли это
+    for (let i = 0; i < newArray.length; i++) {
+      if (newArray[i]) {
+        console.log(true);
+      }
+    }
+    // попробуем
+    console.log(target);
+    await fsAsync.cp("./target/", "./source/", {
+      force: false,
+      recursive: true,
+    });
   },
 };
 
