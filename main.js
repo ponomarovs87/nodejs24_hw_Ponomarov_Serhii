2837f14017099408c32fdaaecb2298a966164a70
evhenious <evhenious@gmail.com>
2024-05-05 17:01:07 +0300

    logger should be imported very simple way

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
-const path = require("path");
-process.env["MY_CONFIG"] = path.join(
-  __dirname,
-  "config",
-  "default.js"
-);
-require(process.env.MY_CONFIG);
-const logger = require(process.env.MY_LOGGER)("main");
+
+const logger = require('./utils/logger')("main");
 const { fileSync } = require("./file_sync");
 
 logger.info("the script is running!");
