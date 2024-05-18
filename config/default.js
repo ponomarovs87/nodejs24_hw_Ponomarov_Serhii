f8c8b3c7420a6b167a83038198a7d3e64d91ca1d
ponomarovs87 <ponomarovs87@gmail.com>
2024-05-18 11:14:44 +0200

    edit config structure

--- config/default.js
+++ config/default.js
@@ -1,9 +1,13 @@
 const useConsoleColors = process.env.COLORS_ENABLED === "1";
 
 module.exports = {
-  serverHost: "http://localhost",
-  serverPort: process.env.SERVER_PORT || 3000,
-  useConsoleColors,
-  logLevel: process.env.LOG_LEVEL || "warn",
-  useOnlyLastLogs: process.env.ONLY_LAST_LOGS || false,
+  server: {
+    host: "http://localhost",
+    port: process.env.SERVER_PORT || 3000,
+  },
+  logger: {
+    useConsoleColors,
+    logLevel: process.env.LOG_LEVEL || "warn",
+    useOnlyLastLogs: process.env.ONLY_LAST_LOGS || false,
+  },
 };
