008a3accb013aca3a4fe40d863178a31852f81a6
ponomarovs87 <ponomarovs87@gmail.com>
2024-05-19 21:15:01 +0200

    add endpoint POST /users putoff POST /user

--- test/task6.express-server.test.js
+++ test/task6.express-server.test.js
@@ -37,99 +37,100 @@ function generateRandomString(characters, maxLetters = 50) {
   return result;
 }
 
-describe("task6(server-express) /user collection", () => {
-  // Test for randomMethod /user
-  for (let i = 0; i < httpMethods.length; i++) {
-    const randomMethod = [httpMethods[i]];
-    if (randomMethod[0] === "post") {
-      it("should return 201 status and 'User created' message", (done) => {
-        const userData = {
-          username: "jonh sMitH",
-          email: "mail2@server.com",
-        };
+//! Включить при необходимости проверки и измения ендпоинта
+// describe("task6(server-express) /user collection", () => {
+//   // Test for randomMethod /user
+//   for (let i = 0; i < httpMethods.length; i++) {
+//     const randomMethod = [httpMethods[i]];
+//     if (randomMethod[0] === "post") {
+//       it("should return 201 status and 'User created' message", (done) => {
+//         const userData = {
+//           username: "jonh sMitH",
+//           email: "mail2@server.com",
+//         };
 
-        supertest(server)
-          .post("/user")
-          .send(userData)
-          .expect(201)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(201);
-            expect(res.text).to.include("User created");
-            done();
-          });
-      });
-      it(`${randomMethod[0]} /user with missing fields should return 400 status and errors`, (done) => {
-        supertest(server)
-          [randomMethod[0]]("/user")
-          .send({})
-          .expect(400)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(400);
-            expect(res.text).to.include("errors");
-            expect(res.text).to.have.include("username");
-            expect(res.text).to.have.include("email");
-            done();
-          });
-      });
-      it(`${randomMethod[0]} /user with missing 'username' fields should return 400 status and errors`, (done) => {
-        supertest(server)
-          [randomMethod[0]]("/user")
-          .send({ email: "mail2@server.com" })
-          .expect(400)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(400);
-            expect(res.text).to.include("errors");
-            expect(res.text).to.have.include("username");
-            done();
-          });
-      });
-      it(`${randomMethod[0]} /user with missing 'email' fields should return 400 status and errors`, (done) => {
-        supertest(server)
-          [randomMethod[0]]("/user")
-          .send({ username: "jonh sMitH" })
-          .expect(400)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(400);
-            expect(res.text).to.include("errors");
-            expect(res.text).to.have.include("email");
-            done();
-          });
-      });
-      it(`${randomMethod[0]} /user with incorrect fields should return 400 status and errors`, (done) => {
-        supertest(server)
-          [randomMethod[0]]("/user")
-          .send({
-            name: "jonh sMitH",
-            mail: "test@mail.test",
-          })
-          .expect(400)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(400);
-            expect(res.text).to.include("errors");
-            expect(res.text).to.have.include("username");
-            expect(res.text).to.have.include("email");
-            done();
-          });
-      });
-    } else {
-      it(`${randomMethod} /user should return 404 status`, (done) => {
-        supertest(server)
-          [httpMethods[i]]("/user")
-          .expect(404)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(404);
-            done();
-          });
-      });
-    }
-  }
-});
+//         supertest(server)
+//           .post("/user")
+//           .send(userData)
+//           .expect(201)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(201);
+//             expect(res.text).to.include("User created");
+//             done();
+//           });
+//       });
+//       it(`${randomMethod[0]} /user with missing fields should return 400 status and errors`, (done) => {
+//         supertest(server)
+//           [randomMethod[0]]("/user")
+//           .send({})
+//           .expect(400)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(400);
+//             expect(res.text).to.include("errors");
+//             expect(res.text).to.have.include("username");
+//             expect(res.text).to.have.include("email");
+//             done();
+//           });
+//       });
+//       it(`${randomMethod[0]} /user with missing 'username' fields should return 400 status and errors`, (done) => {
+//         supertest(server)
+//           [randomMethod[0]]("/user")
+//           .send({ email: "mail2@server.com" })
+//           .expect(400)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(400);
+//             expect(res.text).to.include("errors");
+//             expect(res.text).to.have.include("username");
+//             done();
+//           });
+//       });
+//       it(`${randomMethod[0]} /user with missing 'email' fields should return 400 status and errors`, (done) => {
+//         supertest(server)
+//           [randomMethod[0]]("/user")
+//           .send({ username: "jonh sMitH" })
+//           .expect(400)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(400);
+//             expect(res.text).to.include("errors");
+//             expect(res.text).to.have.include("email");
+//             done();
+//           });
+//       });
+//       it(`${randomMethod[0]} /user with incorrect fields should return 400 status and errors`, (done) => {
+//         supertest(server)
+//           [randomMethod[0]]("/user")
+//           .send({
+//             name: "jonh sMitH",
+//             mail: "test@mail.test",
+//           })
+//           .expect(400)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(400);
+//             expect(res.text).to.include("errors");
+//             expect(res.text).to.have.include("username");
+//             expect(res.text).to.have.include("email");
+//             done();
+//           });
+//       });
+//     } else {
+//       it(`${randomMethod} /user should return 404 status`, (done) => {
+//         supertest(server)
+//           [httpMethods[i]]("/user")
+//           .expect(404)
+//           .end((err, res) => {
+//             expect(err).to.be.null;
+//             expect(res).to.have.status(404);
+//             done();
+//           });
+//       });
+//     }
+//   }
+// });
 
 describe("task6(server-express) /users collection", () => {
   for (let i = 0; i < httpMethods.length; i++) {
@@ -204,6 +205,81 @@ describe("task6(server-express) /users collection", () => {
             });
         });
       }
+    } else if (randomMethod[0] === "post") {
+      it("should return 201 status and 'User created' message", (done) => {
+        const userData = {
+          username: "jonh sMitH",
+          email: "mail2@server.com",
+        };
+
+        supertest(server)
+          .post("/users")
+          .send(userData)
+          .expect(201)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(201);
+            expect(res.text).to.include("User created");
+            done();
+          });
+      });
+      it(`${randomMethod[0]} /user with missing fields should return 400 status and errors`, (done) => {
+        supertest(server)
+          [randomMethod[0]]("/users")
+          .send({})
+          .expect(400)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
+            expect(res.text).to.include("errors");
+            expect(res.text).to.have.include("username");
+            expect(res.text).to.have.include("email");
+            done();
+          });
+      });
+      it(`${randomMethod[0]} /user with missing 'username' fields should return 400 status and errors`, (done) => {
+        supertest(server)
+          [randomMethod[0]]("/users")
+          .send({ email: "mail2@server.com" })
+          .expect(400)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
+            expect(res.text).to.include("errors");
+            expect(res.text).to.have.include("username");
+            done();
+          });
+      });
+      it(`${randomMethod[0]} /user with missing 'email' fields should return 400 status and errors`, (done) => {
+        supertest(server)
+          [randomMethod[0]]("/users")
+          .send({ username: "jonh sMitH" })
+          .expect(400)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
+            expect(res.text).to.include("errors");
+            expect(res.text).to.have.include("email");
+            done();
+          });
+      });
+      it(`${randomMethod[0]} /user with incorrect fields should return 400 status and errors`, (done) => {
+        supertest(server)
+          [randomMethod[0]]("/users")
+          .send({
+            name: "jonh sMitH",
+            mail: "test@mail.test",
+          })
+          .expect(400)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
+            expect(res.text).to.include("errors");
+            expect(res.text).to.have.include("username");
+            expect(res.text).to.have.include("email");
+            done();
+          });
+      });
     } else if (randomMethod[0] === "delete") {
       for (let i = 0; i < randomTestCounter; i++) {
         const validUserId = generateRandomString(
