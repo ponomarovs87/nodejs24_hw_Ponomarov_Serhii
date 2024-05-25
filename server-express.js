64837a7ec08005f9e9b5850879c3b300a902d001
ponomarovs87 <ponomarovs87@gmail.com>
2024-05-25 14:37:14 +0200

    add express client port 3001

--- server-express.js
+++ server-express.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { server } = require("config");
+const { server,client } = require("config");
 
 const express = require("express");
 const {
@@ -16,11 +16,16 @@ const myLogger = require("./utils/logger")(
 );
 
 const app = express();
+const clientApp = express();
 
 app.use(express.json()); // подключение парсера json
 
 app.listen(server.port, () => {
-  myLogger.info(`listening on ${server.port}`);
+  myLogger.info(`listening on ${server.host}:${server.port}`);
+});
+
+clientApp.listen(client.port, () => {
+  myLogger.info(`client listening on ${client.host}:${client.port}`);
 });
 
 [
@@ -31,5 +36,9 @@ app.listen(server.port, () => {
 ].forEach((item) => app.use(item));
 
 app.use("/", routes);
+
+clientApp.get("/",(req,res)=>{
+  res.send({answer:`hi it's port 3001`})
+})
 
 module.exports = app;
